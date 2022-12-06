c537b60204e88ba54c3086f62d8a9dbe8206b5e3
Benevor <2647311844@qq.com>
2022-12-06 19:47:52 +0800

    finish plugin-demo v4

---
+++ plugin-demo/v4/CPluginEnumerator.h
@@ -0,0 +1,19 @@
+#ifndef CPLUGINENUMERATOR_H
+#define CPLUGINENUMERATOR_H
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class CPluginEnumerator
+{
+public:
+    CPluginEnumerator();
+    virtual ~CPluginEnumerator();
+
+    bool GetPluginNames(vector<string>& vstrPluginNames);
+};
+
+#endif
