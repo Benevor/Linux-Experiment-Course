2d92f0ebf7b4aae4368865daef9839cdde173312
Benevor <2647311844@qq.com>
2022-12-24 18:18:36 +0800

    finish serialize Record

--- serialize-string-or-char-demo/serializable.h
+++ serialize-string-or-char-demo/serializable.h
@@ -8,7 +8,8 @@
 enum class_type {
   CLASS_NONE,
   CLASS_FOOD,
-  CLASS_PRODUCT
+  CLASS_PRODUCT,
+  CLASS_RECORD
 };
 
 class SerializableBase {
@@ -178,4 +179,83 @@ class Product : public SerializableBase {
  public:
   int32_t price_;
   std::string name_;
+};
+
+// 交易记录类
+class Record : public SerializableBase {
+ public:
+  Record() {
+    this->user_name_ = nullptr;
+    this->user_name_size_ = 0;
+    this->food_name_ = nullptr;
+    this->food_name_size_ = 0;
+    this->num_ = 0;
+    this->total_price_ = 0;
+  }
+
+  explicit Record(std::string user_name, std::string food_name, int32_t num, int32_t total_price) {
+    this->user_name_size_ = user_name.size();
+    this->food_name_size_ = food_name.size();
+    this->user_name_ = new char[this->user_name_size_];
+    strcpy(this->user_name_, user_name.c_str());
+    this->food_name_ = new char[this->food_name_size_];
+    strcpy(this->food_name_, food_name.c_str());
+    this->num_ = num;
+    this->total_price_ = total_price;
+  }
+
+  virtual ~Record() {
+    delete this->user_name_;
+    delete this->food_name_;
+  }
+
+ public:
+  class_type get_type() override {
+    return CLASS_RECORD;
+  }
+
+  std::string get_info() override {
+    std::stringstream ss;
+    ss << "user: " << this->user_name_ << "  |  food: " << this->food_name_ << "  |  num: " << this->num_
+       << "  |  total_price: " << this->total_price_;
+    std::string str = ss.str();
+    return str;
+  }
+
+  bool serialize(FILE *fp) override {
+    if (fp == nullptr) { return false; }
+    fwrite(&(this->user_name_size_), sizeof(unsigned long), 1, fp);
+    fwrite(this->user_name_, this->user_name_size_, 1, fp);
+
+    fwrite(&(this->food_name_size_), sizeof(unsigned long), 1, fp);
+    fwrite(this->food_name_, this->food_name_size_, 1, fp);
+
+    fwrite(&(this->num_), sizeof(int32_t), 1, fp);
+    fwrite(&(this->total_price_), sizeof(int32_t), 1, fp);
+    return true;
+  }
+
+  SerializableBase *deserialize(FILE *fp) override {
+    auto r = new Record();
+    fread(&(r->user_name_size_), sizeof(unsigned long), 1, fp);
+    r->user_name_ = new char[r->user_name_size_];
+    fread(r->user_name_, r->user_name_size_, 1, fp);
+
+    fread(&(r->food_name_size_), sizeof(unsigned long), 1, fp);
+    r->food_name_ = new char[r->food_name_size_];
+    fread(r->food_name_, r->food_name_size_, 1, fp);
+
+    fread(&(r->num_), sizeof(int32_t), 1, fp);
+    fread(&(r->total_price_), sizeof(int32_t), 1, fp);
+
+    return r;
+  }
+
+ private:
+  char *user_name_;
+  unsigned long user_name_size_;
+  char *food_name_;
+  unsigned long food_name_size_;
+  int32_t num_;
+  int32_t total_price_;
 };
--- serialize-string-or-char-demo/test.cpp
+++ serialize-string-or-char-demo/test.cpp
@@ -1,42 +1,47 @@
 #include "serializable.h"
 
-void se(){
-    Food f1(3, "food1");
-    Product p1(5,"product1");
-    FILE *fp = fopen("data", "w+");
-    if (fp == NULL){return;}
+void se() {
+  Food f1(3, "food1");
+  Product p1(5, "product1");
+  Record r1("user1", "food1", 2, 6);
+  FILE *fp = fopen("data", "w+");
+  if (fp == NULL) { return; }
 
-    class_type type = f1.get_type();
-    fwrite(&type, sizeof(int), 1, fp);
-    f1.serialize(fp);
+  class_type type = f1.get_type();
+  fwrite(&type, sizeof(int), 1, fp);
+  f1.serialize(fp);
 
-    type = p1.get_type();
-    fwrite(&type, sizeof(int), 1, fp);
-    p1.serialize(fp);
+  type = p1.get_type();
+  fwrite(&type, sizeof(int), 1, fp);
+  p1.serialize(fp);
 
-    fclose(fp);
+  type = r1.get_type();
+  fwrite(&type, sizeof(int), 1, fp);
+  r1.serialize(fp);
+
+  fclose(fp);
 }
 
-void de0(){
-    FILE *fp = fopen("data", "r+");
-    if (fp == NULL){return;}
-    class_type nType = CLASS_NONE;
+void de0() {
+  FILE *fp = fopen("data", "r+");
+  if (fp == NULL) { return; }
+  class_type nType = CLASS_NONE;
 
-    int r = fread(&nType, sizeof(int), 1, fp);
-    Food t;
-    auto tmp = (&t)->deserialize(fp);
-    Food* f = dynamic_cast<Food *>(tmp);
-    std::cout<< f->get_info() <<std::endl;
-    delete f;
+  int r = fread(&nType, sizeof(int), 1, fp);
+  Food t;
+  auto tmp = (&t)->deserialize(fp);
+  Food *f = dynamic_cast<Food *>(tmp);
+  std::cout << f->get_info() << std::endl;
+  delete f;
 
-    r = fread(&nType, sizeof(int), 1, fp);
-    Product e;
-    auto temp = (&e)->deserialize(fp);
-    Product* p = dynamic_cast<Product *>(temp);
-    std::cout<< p->get_info() <<std::endl;
-    delete p;
+  r = fread(&nType, sizeof(int), 1, fp);
+  Product e;
+  auto temp = (&e)->deserialize(fp);
+  Product *p = dynamic_cast<Product *>(temp);
+  std::cout << p->get_info() << std::endl;
+  delete p;
 
-    fclose(fp);
+  fclose(fp);
 }
 
 // void de1(){ // 编译不过？？？？
@@ -65,57 +70,63 @@ void de0(){
 //     fclose(fp);
 // }
 
-void de3(){ // 对比 de2()
-    FILE *fp = fopen("data", "r+");
-    if (fp == NULL){return;}
+void de3() { // 对比 de2()
+  FILE *fp = fopen("data", "r+");
+  if (fp == NULL) { return; }
 
-    Food f;
-    f.get_type();
+  Food f;
+  f.get_type();
 
-    fclose(fp);
+  fclose(fp);
 }
 
-void de4(){ // de3()的完善
-    FILE *fp = fopen("data", "r+");
-    if (fp == NULL){return;}
-    class_type nType = CLASS_NONE;
-    int r = fread(&nType, sizeof(int), 1, fp);
+void de4() { // de3()的完善
+  FILE *fp = fopen("data", "r+");
+  if (fp == NULL) { return; }
+  class_type nType = CLASS_NONE;
+  int r = fread(&nType, sizeof(int), 1, fp);
 
-    Food t;
-    auto tmp = t.deserialize(fp);
-    Food* f = dynamic_cast<Food *>(tmp);
-    std::cout<< f->get_info() <<std::endl;
-    delete f;
+  Food t;
+  auto tmp = t.deserialize(fp);
+  Food *f = dynamic_cast<Food *>(tmp);
+  std::cout << f->get_info() << std::endl;
+  delete f;
 
-    r = fread(&nType, sizeof(int), 1, fp);
-    Product e;
-    auto temp = e.deserialize(fp);
-    Product* p = dynamic_cast<Product *>(temp);
-    std::cout<< p->get_info() <<std::endl;
-    delete p;
+  r = fread(&nType, sizeof(int), 1, fp);
+  Product e;
+  auto temp = e.deserialize(fp);
+  Product *p = dynamic_cast<Product *>(temp);
+  std::cout << p->get_info() << std::endl;
+  delete p;
 
-    fclose(fp);
+  r = fread(&nType, sizeof(int), 1, fp);
+  Record m;
+  auto teemp = m.deserialize(fp);
+  Record *r1 = dynamic_cast<Record *>(teemp);
+  std::cout << r1->get_info() << std::endl;
+  delete r1;
+
+  fclose(fp);
 }
 
-void de5(){ // 对比de4， 可以编译，但是运行时会seg fault
-    FILE *fp = fopen("data", "r+");
-    if (fp == NULL){return;}
-    class_type nType = CLASS_NONE;
-    int r = fread(&nType, sizeof(int), 1, fp);
+void de5() { // 对比de4， 可以编译，但是运行时会seg fault
+  FILE *fp = fopen("data", "r+");
+  if (fp == NULL) { return; }
+  class_type nType = CLASS_NONE;
+  int r = fread(&nType, sizeof(int), 1, fp);
 
-    Food* t;
-    auto *p = t->deserialize(fp);
+  Food *t;
+  auto *p = t->deserialize(fp);
 
-    Food* f = dynamic_cast<Food *>(p);
-    std::cout<< f->get_info() <<std::endl;
-    delete f;
-    fclose(fp);
+  Food *f = dynamic_cast<Food *>(p);
+  std::cout << f->get_info() << std::endl;
+  delete f;
+  fclose(fp);
 }
 
-int main()
-{
-    se();
-    de4();
+int main() {
+  se();
+  de4();
 
-    return 0;
+  return 0;
 }
